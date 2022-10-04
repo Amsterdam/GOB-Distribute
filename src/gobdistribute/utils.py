d501e52ee84a0d778cc27286764402927da706d5
Jaap Koetsier <jaap@chariot-it.nl>
2022-10-04 15:21:07 +0200

    Add retry to get_export_products on start of distribute

--- src/gobdistribute/utils.py
+++ src/gobdistribute/utils.py
@@ -1,4 +1,7 @@
 import json
+from requests import Session
+from requests.adapters import HTTPAdapter, Retry
+from gobdistribute.config import EXPORT_API_HOST
 
 
 def json_loads(item):
@@ -7,3 +10,10 @@ def json_loads(item):
     except Exception as e:
         print(f"ERROR: Deserialization failed for item {item}.")
         raise e
+
+
+def get_with_retries(url: str):
+    session = Session()
+    retries = Retry(total=5, backoff_factor=0.5, status_forcelist=[500, 502, 503, 504])
+    session.mount(EXPORT_API_HOST, HTTPAdapter(max_retries=retries))
+    return session.get(url)
--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -1,13 +1,14 @@
 from unittest import mock, TestCase
 
-from gobdistribute.utils import json_loads
+from gobdistribute.utils import json_loads, get_with_retries
+
 
 class TestUtils(TestCase):
-    
+
     @mock.patch('gobdistribute.utils.json')
     def test_json_loads(self, mock_json):
         json_loads('any json')
-        
+
         mock_json.loads.assert_called_with('any json')
 
     @mock.patch('gobdistribute.utils.json')
@@ -15,3 +16,16 @@ class TestUtils(TestCase):
         mock_json.loads.side_effect = Exception('Any Exception')
         with self.assertRaises(Exception):
             mock_json.loads.assert_called_with('any json')
+
+    @mock.patch("gobdistribute.utils.EXPORT_API_HOST", "http://exportapihost")
+    @mock.patch("gobdistribute.utils.Session")
+    @mock.patch("gobdistribute.utils.HTTPAdapter")
+    @mock.patch("gobdistribute.utils.Retry")
+    def test_get_with_retries(self, mock_retry, mock_httpadapter, mock_session):
+        result = get_with_retries("someurl")
+
+        self.assertEqual(result, mock_session.return_value.get.return_value)
+        mock_session.return_value.get.assert_called_with('someurl')
+        mock_session.return_value.mount.assert_called_with("http://exportapihost", mock_httpadapter.return_value)
+        mock_httpadapter.assert_called_with(max_retries=mock_retry.return_value)
+        mock_retry.assert_called_with(total=5, backoff_factor=0.5, status_forcelist=[500, 502, 503, 504])
